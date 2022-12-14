2a0792ff425793c2a548389bd5edf4c16178919d
suhaad79 <111306108+suhaad79@users.noreply.github.com>
2022-12-14 03:25:12 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "app" {
     ami           = "ami-08e637cea2f053dfa"
     instance_type = "t3.micro"
     security_groups = ["jkSG"]
-    key_name      =  "key29"
+    key_name      =  "kops"
     tags = {
         Name = " tomcat-CI"
         Environment = "dev"
